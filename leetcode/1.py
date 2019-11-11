eb8e457699b8d95f1710289338348b390f90f709
Andy Dai <ldai47@uwo.ca>
2019-11-10 19:16:45 -0500

    removed

--- leetcode/1.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def twoSum(self, nums, target):
-        d = {}
-        for i in range(len(nums)):
-            if (target - nums[i]) not in d:
-                d[nums[i]] = i
-            else:
-                return [d[target - nums[i]], i]
--- leetcode/371.py
+++
@@ -1,4 +0,0 @@
-class Solution:
-    def getSum(self, a: int, b: int) -> int:
-        list = [a,b]
-        return sum(list)
